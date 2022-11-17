b72a6531d2cde2a23a44917b201a5a62f2224738
Matheus Alves Pereira <matttalves@gmail.com>
2022-11-17 17:35:19 -0300

    feat: add logo and navigation

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,22 @@
-import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function HomePage() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/types')
+    }, 2000)
+  }, [])
+
   return (
     <div className="App">
       {/* logo */}
       <Link to="/types">
-        <h1>Poke Stats</h1>
+        <h1>
+          <img src="/android-chrome-512x512.png" alt="" />
+        </h1>
       </Link>
     </div>
   )
