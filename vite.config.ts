e36aed7dac29bf3043f9963a5a68789003c03ee4
Matheus Alves Pereira <matttalves@gmail.com>
2022-11-17 17:20:33 -0300

    fix: pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,34 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), VitePWA()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'PokeTypes',
+        short_name: 'PokeTypes',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        theme_color: '#333333',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        lang: 'en',
+      },
+    }),
+  ],
 })
